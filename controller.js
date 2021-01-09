418e3f2c31a32e9ba18b42dcba0a2227fd3f6bd2
harisY <haris.yoboseyo@gmail.com>
2021-01-09 11:51:53 +0700

    add get all karyawan endpoint

--- controller.js
+++ controller.js
@@ -6,3 +6,13 @@ var connection = require('./koneksi');
 exports.index = function (req, res) {
   response.ok('Aplikasi Rest API Koperasi running', res);
 };
+
+exports.GetKaryawan = function (req, res) {
+  connection.query('select * from m_karyawan', function (error, rows, fields) {
+    if (error) {
+      console.log(error);
+    } else {
+      response.ok(rows, res);
+    }
+  });
+};
