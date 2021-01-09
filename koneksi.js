4b77538f055f6d93c2d14be2699a0168eb8e5a1e
harisY <haris.yoboseyo@gmail.com>
2021-01-09 11:00:02 +0700

    configure connection string

--- koneksi.js
+++ koneksi.js
@@ -1,1 +1,15 @@
 var mysql = require('mysql');
+
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '12345',
+  database: 'koperaasiDB',
+});
+
+conn.connect((err) => {
+  if (err) throw err;
+  console.log('Connected');
+});
+
+module.exports = conn;
